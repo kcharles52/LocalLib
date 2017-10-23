1bb81c4f4463ef00f62751b229c58737ae0bcb5d
kcharles52 <kcharles52@gmail.com>
2017-10-23 16:34:43 +0300

    Renaming

--- locallib/routes/index.js
+++ locallib/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 /* //old route
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'New application' });
 });
 */
 
